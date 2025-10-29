350e96436a3f236f6fb8cc22d9e3105ee22039c9
Saurav Bhandari <sauravb1234567@gmail.com>
2025-10-29 10:21:29 +0545

    fixed geometry_sdf to handle cubes correctly

--- simulate.py
+++ simulate.py
@@ -3,8 +3,11 @@ import time
 
 physicsClient = p.connect(p.GUI)
 p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+
+p.loadSDF("box.sdf")
+
 for i in range(1000):
     p.stepSimulation()
-    time.sleep(1/120)
+    time.sleep(1/60)
     print(i)
 p.disconnect()
