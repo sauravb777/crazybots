c5b25ea84da9e77c2d462d7dee5d6d70bc33f972
Saurav Bhandari <sauravb1234567@gmail.com>
2025-11-13 20:21:59 +0545

    synapses updated

--- simulate.py
+++ simulate.py
@@ -1,9 +1,4 @@
-# np.save("data/targetAngles_back.npy", c.targetAngles_back)
-# np.save("data/targetAngles_front.npy", c.targetAngles_front)
-
-# np.save("data/backLegSensorValues.npy", backLegSensorValues)
-# np.save("data/frontLegSensorValues.npy", frontLegSensorValues)
-
 from simulation import SIMULATION
+
 simulation = SIMULATION()
 simulation.Run()
