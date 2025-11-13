edd05cc514998d427c908d9dec08feacc1996382
Saurav Bhandari <sauravb1234567@gmail.com>
2025-11-13 20:07:54 +0545

    .

--- analyze.py
+++ analyze.py
@@ -1,12 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# backLegSensorValues = np.load("data/backLegSensorValues.npy")
-# frontLegSensorValues = np.load("data/frontLegSensorValues.npy")
-
-# plt.plot(backLegSensorValues, label = "Back Leg", linewidth = 3)
-# plt.plot(frontLegSensorValues, label = "Front Leg")
-
 plt.xlabel("Time step")
 plt.ylabel("Target angle (radians)")
 
